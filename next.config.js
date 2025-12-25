55ef89d19a52633324eb9ceefcd6c47f8ec2c779
leebaphatss <lebaphat71@gmail.com>
2025-12-26 01:57:50 +0700

    New Deloy

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const nextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
+  // Base path nếu deploy vào subdirectory (ví dụ: /repository-name)
+  // basePath: process.env.NODE_ENV === 'production' ? '' : '',
 }
 
 module.exports = nextConfig
